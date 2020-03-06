d984d0ec344e0d1c1088e5a96a3213d2ad5e4e31
PussyGirl <55914926+15822422666@users.noreply.github.com>
2020-03-06 08:26:58 +0800

    first commit

---
+++ app/src/main/java/com/example/piaoapplication/BaseWebChromeClient.java
@@ -0,0 +1,28 @@
+package com.example.piaoapplication;
+        import android.view.View;
+        import android.webkit.WebChromeClient;
+
+public class BaseWebChromeClient extends WebChromeClient {
+
+    private IVideo mIVideo;
+
+    public BaseWebChromeClient(IVideo mIVideo) {
+        this.mIVideo = mIVideo;
+    }
+
+    @Override
+    public void onShowCustomView(View view, CustomViewCallback callback) {
+        if (mIVideo != null) {
+            mIVideo.onShowCustomView(view, callback);
+        }
+
+    }
+
+    @Override
+    public void onHideCustomView() {
+        if (mIVideo != null) {
+            mIVideo.onHideCustomView();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/piaoapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.piaoapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.main_webView);
+        //支持javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 设置可以支持缩放
+        webView.getSettings().setSupportZoom(true);
+        // 设置出现缩放工具
+        webView.getSettings().setBuiltInZoomControls(true);
+        //扩大比例的缩放
+        webView.getSettings().setUseWideViewPort(true);
+        //自适应屏幕
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.setWebChromeClient(new BaseWebChromeClient(new VideoImpl(this, webView)));
+
+        //如果不设置WebViewClient，请求会跳转系统浏览器
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //该方法在Build.VERSION_CODES.LOLLIPOP以前有效，从Build.VERSION_CODES.LOLLIPOP起，建议使用shouldOverrideUrlLoading(WebView, WebResourceRequest)} instead
+                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+
+                if (url.toString().contains("sina.cn")){
+                    view.loadUrl("http://tv.itnote.vip:1314");
+                    return true;
+                }
+
+                return false;
+            }
+
+        });
+        webView.loadUrl("http://tv.itnote.vip:1314");
+    }
+}
+
